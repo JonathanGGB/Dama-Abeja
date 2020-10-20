60698eccab3d670ae2c2cfa6c6ce9151df2d1580
JonathanGGB <56163813+JonathanGGB@users.noreply.github.com>
2020-10-20 17:26:30 -0500

    Update menu.js

--- js/menu.js
+++ js/menu.js
@@ -9,13 +9,6 @@ function main() {
                 left: '0'
             });
             contador = 0;
-            $('ul').click(function() {
-                $('nav').animate({
-                    left: '-100%'
-                });
-                var auxiliar = document.getElementById("lista").text;
-                document.getElementById("cabecera").href = '#' + auxiliar;
-            })
         } else {
             contador = 1;
             $('nav').animate({
